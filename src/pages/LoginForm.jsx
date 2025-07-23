af8332745b086488a80ddf096380afdf9b40c570
laurenannmurugi <laurenann1871@gmail.com>
2025-07-24 00:41:49 +0300

    feat: implement handleSubmit function for user authentication in LoginForm component

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -17,9 +17,21 @@ const toggleForm = () => {
     setPassword('');
   };
 
-  const handleSubmit = async (e) => {
+ const handleSubmit = async (e) => {
     e.preventDefault();
     const endpoint = 'http://localhost:3000/users';
 
-  }
-};
+    if (isLogin) {
+      const res = await fetch(`${endpoint}?email=${email}&password=${password}`);
+      const users = await res.json();
+      if (users.length > 0) {
+        localStorage.setItem('token', 'loggedin');
+        localStorage.setItem('user', JSON.stringify(users[0]));
+        navigate('/'); // Redirect to Home
+      } else {
+        alert('Invalid email or password');
+      }
+    } 
+
+ }
+}
