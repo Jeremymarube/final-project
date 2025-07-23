bc4f5b4c7e26dfde36158d1876880364fc83baf3
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-23 23:34:10 +0300

    implement StudentList component with student fetching logic

--- src/pages/StudentList.jsx
+++ src/pages/StudentList.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const StudentList = () => {
+  const [students, setStudents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    fetch("http://localhost:3000/students")
+      .then((res) => res.json())
+      .then(setStudents);
+  }, []);
+}
 export default StudentList;
